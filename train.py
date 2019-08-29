c622bc4b331409aa01647f3fac6bda717e5d8236
bunthet01 <bunthetsay@gmail.com>
2019-08-30 00:34:57 +0200

    done

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 
 # load data
@@ -14,6 +15,14 @@ x_train = x_train/255.0
 x_test = x_test/255.0
 
 
+checkpoint_path = "training_2/cp-{epoch:04d}.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create checkpoint callback
+cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1, period=2)
+
 # define model
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
@@ -21,16 +30,21 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(10, activation=tf.nn.softmax)
     ])
+
+model.summary()
 # compile the model
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
 # train
-loss_history = model.fit(x_train, y_train, epochs=3, batch_size=128, verbose=1, validation_split=0.2, shuffle=True,
-                         workers=4, use_multiprocessing=True, max_queue_size=4)
+model.save_weights(checkpoint_path.format(epoch=0))
+loss_history = model.fit(x_train, y_train, epochs=10, batch_size=128, verbose=1, validation_split=0.2, shuffle=True,
+                         workers=4, use_multiprocessing=True, max_queue_size=4, callbacks=[cp_callback])
 _, test_accu = model.evaluate(x_test, y_test)
 print("Test accuracy: ", test_accu)
+
+
 
 # plot
 for i in range(10):
@@ -41,6 +55,9 @@ for i in range(10):
     plt.imshow(x_test[i], cmap=plt.cm.binary)
     plt.xlabel(np.argmax(model.predict(x_test[i][None, ...])))
 plt.show()
+
+
+
 
 
 
